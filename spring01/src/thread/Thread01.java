a3678003db92bde1a72985871902251b12ddc2d5
shuqi <solrs@sina.cn>
2018-05-03 11:25:49 +0800

    spring提交

---
+++ spring01/src/thread/Thread01.java
@@ -0,0 +1,29 @@
+package thread;
+
+import com.sun.istack.internal.Nullable;
+import org.omg.CORBA.LocalObject;
+
+/**
+ * Created by shuqi on 2018/4/25.
+ */
+public class Thread01  extends  Thread{
+    int i;
+
+    @Override
+    public void run() {
+
+    }
+    protected  static int k =0;
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++) {
+            new Thread("" + i) {
+                @Override
+                public void run() {
+                    k = k + 3;
+                    System.out.println("哈哈哈" + (k));
+                }
+            }.start();
+        }
+    }
+
+}
